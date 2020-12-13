94ef498ea7e4884fc69ee6c818b20d20997abaca
bob_home <chanmin9401@gmail.com>
2020-12-13 11:38:52 +0900

    modify img src url + title image resize

--- src/MyProfile/MyProfile.js
+++ src/MyProfile/MyProfile.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './MyProfile.css';
 
 const MyProfile = ({form, children}) => {
+    var global = "https://chanmin9401.github.io/myprofile-page";
+    //global = localhost:3000?
     return (
         <main className="myprofile">
             <div className="titleArea">
                 <div className="titleImage">
-                    <img alt="titleImg" className="img"
-                        src="/image/profile_image.jpg">
+                    <img alt="titleImg" className="img" src= {global+"/image/profile_image.jpg"}>
+                    {/* <img alt="titleImg" className="img" src="myprofile-page/image/profile_image.jpg"> */}
                     </img>
                 </div>
                 <div className="titleWords">
