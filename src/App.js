ead1cce95b6ee16b7126c622c0de6499dba26039
bob_home <chanmin9401@gmail.com>
2020-12-16 23:35:50 +0900

    fix unexpected syntax

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css'
 
 class App extends Component{
   profileInfo = {
-    global = "https://chanmin9401.github.io/myprofile-page",
+    global : "https://chanmin9401.github.io/myprofile-page",
     // global : "http://localhost:3000/myprofile-page",
     myName : "Dev. BOB",
     myIntoduce : "Junior F/E Developer.",
