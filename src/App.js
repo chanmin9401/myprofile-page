5e2db3c14de92fa1121133d386540bb95a8fd297
bob_home <chanmin9401@gmail.com>
2020-12-16 23:19:07 +0900

    add Footer Component and parameters for MyProfile Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import MyProfile from './MyProfile/MyProfile';
+import MyProfileFooter from './MyProfile/MyProfileFooter';
+import './App.css'
 
 class App extends Component{
+  profileInfo = {
+    //global = "https://chanmin9401.github.io/myprofile-page",
+    global : "http://localhost:3000/myprofile-page",
+    myName : "Dev. BOB",
+    myIntoduce : "Junior F/E Developer.",
+    career : "SINCE. 18.01"
+  }
+
   render() {
       return (
-        <MyProfile>
-          App
-        </MyProfile>
+        <div className ="contents">
+          <MyProfile data={this.profileInfo}></MyProfile>
+          <MyProfileFooter></MyProfileFooter>
+        </div>
       );
   }
 }
