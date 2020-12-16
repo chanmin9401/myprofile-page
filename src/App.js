0d8fae131c04a157dc138e93df309cd7c3a862de
bob_home <chanmin9401@gmail.com>
2020-12-16 23:34:00 +0900

    modify global url for image

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import './App.css'
 
 class App extends Component{
   profileInfo = {
-    //global = "https://chanmin9401.github.io/myprofile-page",
-    global : "http://localhost:3000/myprofile-page",
+    global = "https://chanmin9401.github.io/myprofile-page",
+    // global : "http://localhost:3000/myprofile-page",
     myName : "Dev. BOB",
     myIntoduce : "Junior F/E Developer.",
     career : "SINCE. 18.01"
